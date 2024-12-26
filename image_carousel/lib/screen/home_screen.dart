793ceab8c1fca4a812e80f1a1e9bf08cfed3c3d2
MinGyu <kimmingyu9811@gmail.com>
2024-12-26 23:35:53 +0900

    image carousel app

---
+++ image_carousel/lib/screen/home_screen.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(
+        Duration(
+          seconds: 2,
+        ), (timer) {
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1 > 4 ? 0 : currentPage + 1;
+
+      controller.animateToPage(nextPage,
+          duration: Duration(milliseconds: 500), curve: Curves.linear);
+    });
+  }
+
+  @override
+  void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        children: [1, 2, 3, 4, 5]
+            .map(
+              (e) => Image.asset(
+                'asset/img/image_$e.jpeg',
+                fit: BoxFit.cover,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
