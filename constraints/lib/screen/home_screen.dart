9fc60511cd043649f5e366d0bd30cbc1a499784d
MinGyu <kimmingyu9811@gmail.com>
2024-12-29 23:59:25 +0900

    alignment study

--- constraints/lib/screen/home_screen.dart
+++ constraints/lib/screen/home_screen.dart
@@ -11,14 +11,14 @@ class HomeScreen extends StatelessWidget {
           height: 200.0,
           width: 200.0,
           color: Colors.red,
-          child: Row(
-            children: [
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.blue,
-              ),
-            ],
+          child: Align(
+            // alignment: Alignment.bottomRight,
+            alignment: Alignment(1, 0.5),
+            child: Container(
+              height: 50.0,
+              width: 50.0,
+              color: Colors.blue,
+            ),
           ),
         ),
       ),
