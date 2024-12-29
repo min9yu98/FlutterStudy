bbebc7b7afcd2982b5ebc8f5d853110fab2e9028
MinGyu <kimmingyu9811@gmail.com>
2024-12-29 23:54:30 +0900

    constraint study

---
+++ constraints/lib/screen/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 200.0,
+          width: 200.0,
+          color: Colors.red,
+          child: Row(
+            children: [
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
